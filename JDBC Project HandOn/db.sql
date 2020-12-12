92d0c28674763b13b470123baa3cb2303f00922c
Rahul Chauhan <rchauhan9102@gmail.com>
2020-12-12 19:13:59 +0530

    Deleting all folder as those were not structured

--- JDBC Project HandOn/db.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE subject;
-DROP TABLE student;
-CREATE TABLE subject(id int PRIMARY KEY AUTO_INCREMENT, name varchar(25));
-CREATE TABLE student(id int PRIMARY KEY AUTO_INCREMENT, student_name varchar(20), score float, subject_id int references subject(id));
--- README.md
+++
@@ -1,2 +0,0 @@
-# Rahul-REPO
-This repository contains some JAVA problems and JAVA technologies like JDBC,Hibernate, Spring et.c
